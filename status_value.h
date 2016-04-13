c44d8025b118979f9fafe796cdb364c1c891e5ac
JF Bastien <jfb@chromium.org>
2016-04-13 15:59:16 -0700

    Basics.

---
+++ status_value.h
@@ -0,0 +1,12 @@
+// Licensed under the Apache License, Version 2.0.
+
+#ifndef STATUS_VALUE_H
+#define STATUS_VALUE_H
+
+namespace experimental {
+
+
+
+}  // namespace experimental
+
+#endif
---
+++ test.cc
@@ -0,0 +1,7 @@
+// Licensed under the Apache License, Version 2.0.
+
+#include "status_value.h"
+
+int main() {
+  return 0;
+}
