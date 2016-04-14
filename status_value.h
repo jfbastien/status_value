8a01a00b741d4566aa747f1d1d1e6f62a358160b
JF Bastien <jfb@chromium.org>
2016-04-13 19:36:01 -0700

    Basic implementation.

--- status_value.h
+++ status_value.h
@@ -3,9 +3,36 @@
 #ifndef STATUS_VALUE_H
 #define STATUS_VALUE_H
 
+#include <utility>
+
 namespace experimental {
 
+template <typename Status, typename Value>
+class status_value {
+  Status s;
+  Value v;
+  bool p : 1;
+  status_value() = delete;
 
+ public:
+  status_value(Status s) : s(s), v(), p(false) {}
+  status_value(Status s, Value&& v) : s(s), v(std::move(v)), p(true) {}
+  status_value(Status s, const Value& v) : s(s), v(v), p(true) {}
+  status_value(status_value&&) = default;
+  const Status& status() const { return s; }
+  bool has_value() const { return p; }
+  operator bool() const { return p; }
+  const Value& value() const {
+    if (p) return v;
+    throw s;
+  }
+  Value& value() {
+    if (p) return v;
+    throw s;
+  }
+  const Value& operator*() const { return value(); }
+  Value& operator*() { return value(); }
+};
 
 }  // namespace experimental
 
--- test.cc
+++ test.cc
@@ -1,7 +1,16 @@
 // Licensed under the Apache License, Version 2.0.
 
+#include <system_error>
 #include "status_value.h"
 
+template <typename Value>
+using Maybe = experimental::status_value<std::error_code, Value>;
+
+void test() {
+  Maybe<int> i((std::error_code()));
+}
+
 int main() {
+  test();
   return 0;
 }
